7365923003d3404a4a85b73176c94437b9d45fb9
Ivan Filon <vanbest27@gmail.com>
2025-02-08 20:12:37 +0300

    DONE: searching by parameters

--- src/main/java/com/example/demo/repository/HotelRepository.java
+++ src/main/java/com/example/demo/repository/HotelRepository.java
@@ -2,8 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+    @Query("select hotel from Hotel hotel where " +
+            "(:name is NULL or hotel.name like :name) and " +
+            "(:brand is NULL or hotel.brand LIKE :brand) and " +
+            "(:city is NULL or hotel.address.city like :city) and " +
+            "(:country is NULL or hotel.address.country like :country) and " +
+            "(:amenities is NULL or EXISTS (select am from hotel.amenities am where am.name in :amenities))")
+    List<Hotel> findBySearchCriteria(@Param("name") String name, @Param("brand") String brand, @Param("city") String city, @Param("country") String country, @Param("amenities") List<String> amenities);
 }
